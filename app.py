ac1e597baf0f3a32e2f51205f336b3be2257eb96
J. Sebastian Paez <jspaezp@gmail.com>
2023-05-03 14:36:30 -0700

    updated

--- app.py
+++ app.py
@@ -12,29 +12,37 @@ with st.sidebar:
     lookup_text = st.text_area("Enter Query SMILES here", height=100, key="query_smiles", value=default_smiles)
 
     smiles = [x.strip() for x in text.split("\n")]
-    query_smiles = [x.strip() for x in text.split("\n")]
+    query_smiles = [x.strip() for x in lookup_text.split("\n")]
 
-    canon_smiles = [Chem.CanonSmiles(smile) for smile in smiles]
-    canon_query_smiles = [Chem.CanonSmiles(smile) for smile in query_smiles]
+    canon_smiles = [Chem.CanonSmiles(smile) for smile in smiles if smile]
+    canon_query_smiles = [Chem.CanonSmiles(smile) for smile in query_smiles if smile]
 
 mols = [Chem.MolFromSmiles(smile) for smile in canon_smiles]
-imgs = [Draw.MolToImage(mol) for mol in mols]
+imgs = [Draw.MolToImage(mol, size=(600,400)) for mol in mols]
 
 for i, (img, smi, csmi) in enumerate(zip(imgs, smiles, canon_smiles)):
     if smi != csmi:
-        caption = f"Original: {smi}\nCanonical: {csmi}"
+        caption = f"Original: {smi}\n\nCanonical: {csmi}"
     else:
         caption = smi
 
     with st.expander(f"{i+1}: {smi}"):
-        st.image(img, width=200, caption=caption)
+        st.markdown(f"```\n{caption}\n```")
+        st.image(img, width=600)
+
+match_positions = []
+for x in canon_smiles:
+    try:
+        match_positions.append(canon_query_smiles.index(x))
+    except ValueError:
+        match_positions.append(None)
 
 
 df = pd.DataFrame({
     'Input SMILES': smiles,
     'Canonical SMILES': canon_smiles,
     'is cannonical': [x == y for x, y in zip(smiles, canon_smiles)],
-    'is in query': [x in canon_query_smiles for x in canon_smiles],
+    'query position': match_positions,
 })
 
 st.dataframe(df)
