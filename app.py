16127b0d1673764a3c87f9f8f7b10866123b57fb
J. Sebastian Paez <jspaezp@gmail.com>
2023-05-03 16:16:19 -0700

    added compound similarity

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 
+import numpy as np
 import pandas as pd
 import streamlit as st
 import rdkit
 from rdkit.Chem import Draw
 from rdkit import Chem
+from rdkit.Chem.Fingerprints import FingerprintMols
+from rdkit import DataStructs
+import altair as alt
 
 with st.sidebar:
     default_smiles = "\n".join(["CN2C(=O)N(C)C(=O)C1=C2N=CN1C",
@@ -18,6 +22,7 @@ with st.sidebar:
     canon_query_smiles = [Chem.CanonSmiles(smile) for smile in query_smiles if smile]
 
 mols = [Chem.MolFromSmiles(smile) for smile in canon_smiles]
+query_mols = [Chem.MolFromSmiles(smile) for smile in canon_query_smiles]
 imgs = [Draw.MolToImage(mol, size=(600,400)) for mol in mols]
 
 for i, (img, smi, csmi) in enumerate(zip(imgs, smiles, canon_smiles)):
@@ -53,3 +58,20 @@ st.download_button(
     file_name="canonical_smiles.csv",
     mime="text/csv",
 )
+
+st.markdown("# Similarity Matrix")
+
+target_fps = [FingerprintMols.FingerprintMol(x) for x in mols]
+query_fps = [FingerprintMols.FingerprintMol(x) for x in query_mols]
+
+similarity_matrix = [[DataStructs.FingerprintSimilarity(x,y) for x in target_fps] for y in query_fps]
+x, y = np.meshgrid(range(1, len(target_fps) + 1), range(1, len(query_fps) + 1))
+sim_array = np.array(similarity_matrix)
+chart_df = pd.DataFrame({'target': x.ravel(), 'query': y.ravel(), 'similarity': sim_array.ravel()})
+chart = alt.Chart(chart_df).mark_rect().encode(
+    x='target:O',
+    y='query:O',
+    color='similarity:Q'
+)
+
+st.altair_chart(chart, use_container_width=True)
